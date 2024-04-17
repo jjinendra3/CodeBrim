2f33eebaee954fa543fcacd46949f7a605161321
Jinendra Jain <jjinendra3@gmail.com>
2024-04-17 20:58:27 +0530

    console logging for debugging

--- Backend/routes/code.ts
+++ Backend/routes/code.ts
@@ -5,13 +5,17 @@ import * as fs from "fs";
 const app = Router();
 const date = new Date();
 function buildDockerImage(lang: string) {
+  console.log("Building Docker Image");
   return new Promise((resolve, reject) => {
+    console.log("Building Docker Image2");
     const dockerCommand = `docker build -t my-${lang}-app ./dock/${lang}`;
     exec(dockerCommand, (error: any, stdout: any, stderr: any) => {
       if (error) {
+        console.log({ stderr, error });
         reject({ stderr, error });
         return;
       }
+      console.log(stdout);
       resolve(stdout);
     });
   });
@@ -19,20 +23,25 @@ function buildDockerImage(lang: string) {
 
 function runImage(lang: string, stdin: string) {
   return new Promise((resolve, reject) => {
+    console.log("Running Docker Image");
     const dockerRunCommand = `docker run -i my-${lang}-app`;
     const child = exec(
       dockerRunCommand,
       (error: any, stdout: any, stderr: any) => {
         if (error) {
+          console.log({ error, stderr });
           reject({ error, stderr });
           return;
         }
+        console.log(stdout);
         resolve(stdout);
       },
     );
+    console.log("Running Docker Image2 Interactively");
     stdin = stdin === undefined || stdin === null ? (stdin = "") : stdin;
     child.stdin.write(stdin);
     child.stdin.end();
+    console.log('Running container for interactive console.')
   });
 }
 function findClassName(javaCode: string): string | null {
@@ -42,6 +51,7 @@ function findClassName(javaCode: string): string | null {
 }
 app.post("/runcode", async (req: any, res: any) => {
   //run project save then run code
+  console.log(1);
   const lang: string = req.body.files.lang;
   let ext: string;
   if (lang === "cpp" || lang === "java" || lang === "go") {
@@ -55,6 +65,7 @@ app.post("/runcode", async (req: any, res: any) => {
   } else {
     return res.send({ success: 0, error: "Language not chosen" });
   }
+  console.log(1);
   if (lang === "java") {
     const javafilename = findClassName(req.body.files.content);
     fs.writeFileSync(
@@ -68,9 +79,13 @@ app.post("/runcode", async (req: any, res: any) => {
   } else {
     fs.writeFileSync(`./dock/${lang}/main.${ext}`, req.body.files.content);
   }
+  console.log(2);
   try {
+    console.log(3);
     await buildDockerImage(lang);
+    console.log(4);
   } catch (error: any) {
+    console.log({ error });
     if (lang === "cpp" || lang === "java") {
       const originalString = error.stderr;
       const firststring = "[4/4]";
@@ -84,6 +99,7 @@ app.post("/runcode", async (req: any, res: any) => {
               index - 24,
             )
           : originalString;
+          console.log(copiedString,'error1');
       await prisma.files.update({
         where: {
           id: req.body.files.id,
@@ -114,6 +130,7 @@ app.post("/runcode", async (req: any, res: any) => {
           "Failure in compiling the code, please try again later.",
       },
     });
+    console.log(''+error,'error3')
     return res.send({
       success: 0,
       stdout:
@@ -124,7 +141,9 @@ app.post("/runcode", async (req: any, res: any) => {
     });
   }
   try {
+    console.log('running image')
     const Runner: any = await runImage(lang, req.body.files.stdin);
+    console.log('image ran')
     await prisma.files.update({
       where: {
         id: req.body.files.id,
@@ -134,9 +153,11 @@ app.post("/runcode", async (req: any, res: any) => {
         stdout: date + ">>>\n" + Runner,
       },
     });
+    console.log('updated');
     return res.send({ success: 1, stdout: date + ">>>\n" + Runner });
   } catch (error: any) {
     const err = error.stderr.toString();
+    console.log(error,err,"running image error");
     await prisma.files.update({
       where: {
         id: req.body.files.id,
@@ -146,6 +167,7 @@ app.post("/runcode", async (req: any, res: any) => {
         stdout: date + ">>>\n" + err,
       },
     });
+    console.log('updated',err);
     return res.send({
       success: 0,
       stdout: date + ">>>\n" + error.stderr,
